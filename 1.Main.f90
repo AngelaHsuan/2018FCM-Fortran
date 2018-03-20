8ff5ec8b21e925f0e203f5e1769d6c48e23c302a
AngelaHsuan <36875859+AngelaHsuan@users.noreply.github.com>
2018-03-20 23:53:32 +0800

    1.Main

---
+++ 1.Main.f90
@@ -0,0 +1,32 @@
+program Main
+    implicit NONE
+    
+    integer :: choose
+    character :: again
+    
+    again = 'y'
+    
+    do while (again == 'y')
+    
+        write(*,*)'Which programing time do you want to know?'
+        write(*,*)'(1) addition'
+        write(*,*)'(2) multiplication'
+        write(*,*)'(3) sine function'
+        read(*,*) choose
+    
+        if (choose == 1) then
+            call addition
+        else if (choose == 2) then
+            call multiplication
+        else if (choose == 3) then
+            call sine
+        end if
+        
+        write(*,*)''
+        write(*,*)'Do you want to do it again?(y/n)'
+        read(*,*)again
+        
+    end do
+    pause
+    
+end program Main
